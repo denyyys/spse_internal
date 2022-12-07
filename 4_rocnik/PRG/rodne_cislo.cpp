8f5d71af393099cef6a78175415329ef739a6671
denyyys <94078181+denyyys@users.noreply.github.com>
2022-12-07 13:59:05 +0100

    update rodne_cislo check

--- 4_rocnik/PRG/rodne_cislo.cpp
+++ 4_rocnik/PRG/rodne_cislo.cpp
@@ -1,5 +1,6 @@
 // rodne cislo 05-09-31/1871
 // autor: ja + atro
+// bug: blbe vypisuje mesic
 
 #include <iostream>
 #include <string>
@@ -15,13 +16,19 @@ int main()
 	cout << "Zadej rodne cislo: ";
 	cin >> r_cislo;
 	
+	if((r_cislo.size()==10) && (stoll(r_cislo)%11)==0){
+		cout << "Platne rodne cislo" << endl;
+	}else{
+		cout << "Neplatne rodne cislo" << endl;
+		exit(1);
+	}
+	
 	if ((r_cislo[2] >= 6)){
 		pohlavi = "zena";
 		}else{
 		pohlavi = "muz";
 	}
-		
-
+	
 	
 	
 	cout << "Rok: " << r_cislo[0] << r_cislo[1] << endl;
